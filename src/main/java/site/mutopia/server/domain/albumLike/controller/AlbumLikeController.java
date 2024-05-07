466a8c66a6f53d20b11947830732fa0e60da5cee
jihoonwang98 <jihoonwang98@naver.com>
2024-05-07 23:00:24 +0900

    refactor: modify endpoint to POST /album/{albumId}/like/toggle

--- src/main/java/site/mutopia/server/domain/albumLike/controller/AlbumLikeController.java
+++ src/main/java/site/mutopia/server/domain/albumLike/controller/AlbumLikeController.java
@@ -2,21 +2,18 @@ package site.mutopia.server.domain.albumLike.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import site.mutopia.server.domain.albumLike.service.AlbumLikeService;
 
 @RestController
-@RequestMapping("/album-like")
+@RequestMapping("/album")
 @RequiredArgsConstructor
 public class AlbumLikeController {
 
     private final AlbumLikeService albumLikeService;
 
-    @PostMapping("/toggle")
-    public ResponseEntity<?> toggleLike(@RequestParam("albumId") String albumId, @RequestParam("userId") String userId) {
+    @PostMapping("/{albumId}/like/toggle")
+    public ResponseEntity<?> toggleLike(@PathVariable("albumId") String albumId, @RequestParam("userId") String userId) {
         albumLikeService.toggleAlbumLike(albumId, userId);
         return ResponseEntity.ok().build();
     }
