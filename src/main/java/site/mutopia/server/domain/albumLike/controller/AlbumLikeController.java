9dcff58d7810dc390606f6370a8e07eb971340fc
jihoonwang98 <jihoonwang98@naver.com>
2024-05-07 23:06:33 +0900

    fix: use @LoginUser's userId instead of @RequestParam's userId

--- src/main/java/site/mutopia/server/domain/albumLike/controller/AlbumLikeController.java
+++ src/main/java/site/mutopia/server/domain/albumLike/controller/AlbumLikeController.java
@@ -4,6 +4,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import site.mutopia.server.domain.albumLike.service.AlbumLikeService;
+import site.mutopia.server.domain.auth.annotation.LoginUser;
+import site.mutopia.server.domain.user.entity.UserEntity;
 
 @RestController
 @RequestMapping("/album")
@@ -13,8 +15,8 @@ public class AlbumLikeController {
     private final AlbumLikeService albumLikeService;
 
     @PostMapping("/{albumId}/like/toggle")
-    public ResponseEntity<?> toggleLike(@PathVariable("albumId") String albumId, @RequestParam("userId") String userId) {
-        albumLikeService.toggleAlbumLike(albumId, userId);
+    public ResponseEntity<?> toggleLike(@PathVariable("albumId") String albumId, @LoginUser UserEntity loggedInUser) {
+        albumLikeService.toggleAlbumLike(albumId, loggedInUser.getId());
         return ResponseEntity.ok().build();
     }
 }
