6f5cdddde72cba0ac854b26cc73a7167d756b7bc
jihoonwang98 <jihoonwang98@naver.com>
2024-05-07 20:25:10 +0900

    refactor: throw not found exception in service layer

--- src/main/java/site/mutopia/server/domain/album/repository/AlbumRepository.java
+++ src/main/java/site/mutopia/server/domain/album/repository/AlbumRepository.java
@@ -4,10 +4,11 @@ package site.mutopia.server.domain.album.repository;
 import site.mutopia.server.domain.album.entity.AlbumEntity;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface AlbumRepository {
 
-    AlbumEntity findAlbumById(String albumId);
+    Optional<AlbumEntity> findAlbumById(String albumId);
 
     List<AlbumEntity> findAlbumByAlbumName(String albumName);
 
