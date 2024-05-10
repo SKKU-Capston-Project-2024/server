21d998a0477b5b47604e94119b95b624d1da5f51
jihoonwang98 <jihoonwang98@naver.com>
2024-05-10 11:53:53 +0900

    feat: GET album reviews by userId

--- src/main/java/site/mutopia/server/domain/user/controller/UserController.java
+++ src/main/java/site/mutopia/server/domain/user/controller/UserController.java
@@ -2,10 +2,9 @@ package site.mutopia.server.domain.user.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import site.mutopia.server.domain.albumReview.dto.AlbumReviewInfoDto;
+import site.mutopia.server.domain.albumReview.service.AlbumReviewService;
 import site.mutopia.server.domain.auth.annotation.LoginUser;
 import site.mutopia.server.domain.topster.dto.TopsterInfoDto;
 import site.mutopia.server.domain.topster.service.TopsterService;
@@ -13,6 +12,7 @@ import site.mutopia.server.domain.user.dto.UserAggregationInfoResDto;
 import site.mutopia.server.domain.user.entity.UserEntity;
 import site.mutopia.server.domain.user.service.UserService;
 import site.mutopia.server.swagger.response.NotFoundResponse;
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -21,6 +21,7 @@ public class UserController {
 
     private final UserService userService;
     private final TopsterService topsterService;
+    private final AlbumReviewService albumReviewService;
 
     @GetMapping("/info")
     public ResponseEntity<UserEntity> getUserInfo(@LoginUser UserEntity userEntity) {
@@ -38,5 +39,12 @@ public class UserController {
     public ResponseEntity<TopsterInfoDto> getUserTopster(@PathVariable("userId") String userId) {
         TopsterInfoDto topster = topsterService.getTopsterInfoByUserId(userId);
         return ResponseEntity.ok().body(topster);
+    }
+
+    @GetMapping("/{userId}/album/review")
+    public ResponseEntity<List<AlbumReviewInfoDto>> getUserAlbumReview(@PathVariable("userId") String userId,
+                                               @RequestParam(value = "limit", required = false, defaultValue = "10") Integer limit) {
+        List<AlbumReviewInfoDto> reviews = albumReviewService.findAlbumReviewInfoDtoListByUserId(userId, limit);
+        return ResponseEntity.ok(reviews);
     }
 }
