f7b69ac805b53f7adbbbcaa66ca39dace8102815
jihoonwang98 <jihoonwang98@naver.com>
2024-05-10 22:48:12 +0900

    feat: GET playlistInfo by userId

--- src/main/java/site/mutopia/server/domain/user/controller/UserController.java
+++ src/main/java/site/mutopia/server/domain/user/controller/UserController.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.*;
 import site.mutopia.server.domain.albumReview.dto.AlbumReviewInfoDto;
 import site.mutopia.server.domain.albumReview.service.AlbumReviewService;
 import site.mutopia.server.domain.auth.annotation.LoginUser;
+import site.mutopia.server.domain.playlist.dto.PlaylistInfoDto;
+import site.mutopia.server.domain.playlist.service.PlaylistService;
 import site.mutopia.server.domain.songComment.dto.SongCommentInfoResDto;
 import site.mutopia.server.domain.songComment.service.SongCommentService;
 import site.mutopia.server.domain.topster.dto.TopsterInfoDto;
@@ -25,6 +27,7 @@ public class UserController {
     private final TopsterService topsterService;
     private final AlbumReviewService albumReviewService;
     private final SongCommentService songCommentService;
+    private final PlaylistService playlistService;
 
     @GetMapping("/info")
     public ResponseEntity<UserEntity> getUserInfo(@LoginUser UserEntity userEntity) {
@@ -56,5 +59,13 @@ public class UserController {
                                                  @RequestParam(value = "limit", required = false, defaultValue = "10") Integer limit) {
         List<SongCommentInfoResDto> userSongComments = songCommentService.getUserSongComments(userId, limit);
         return ResponseEntity.ok().body(userSongComments);
+    }
+
+    // TODO: Playlist 기능 구현하고 Test 하기
+    @GetMapping("/{userId}/playlist")
+    public ResponseEntity<List<?>> getUserPlaylist(@PathVariable("userId") String userId,
+                                                   @RequestParam(value = "limit", required = false, defaultValue = "10") Integer limit) {
+        List<PlaylistInfoDto> userPlaylists = playlistService.getUserPlaylists(userId, limit);
+        return ResponseEntity.ok().body(userPlaylists);
     }
 }
